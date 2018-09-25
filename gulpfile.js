c4f4f886f20c038ff990a59c418dfaf78dd64136
maitiantian <876633545@qq.com>
2018-09-25 09:39:31 +0800

    初始状态

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ browserSync.init({
 });
 
 gulp.task('default', function(){
-	gulp.watch("./src/index.html").on('change', reload);
+	gulp.watch("./src/*.*").on('change', reload);
 	gulp.watch("./src/css/*.*").on('change', reload);
 	gulp.watch("./src/scripts/*.*").on('change', reload);
 });
